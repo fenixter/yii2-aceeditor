b95cd11a0f834d2982f82b0cf7e4ee96bd3c612e
Oleksii Fedoryshyn <fenixter@gmail.com>
2016-12-12 12:53:26 +0200

    Fix path

--- src/AceEditorAsset.php
+++ src/AceEditorAsset.php
@@ -14,7 +14,7 @@ class AceEditorAsset extends AssetBundle
     /**
      * @inheritdoc
      */
-    public $sourcePath = '@bower/ace-builds/src-min-noconflict';
+    public $sourcePath = __DIR__ . '/../../../bower-asset/ace-builds/src-min-noconflict';
 
     /**
      * @inheritdoc
